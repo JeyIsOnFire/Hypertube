ecb86fd8c61311101a6ab42946f821d715daee51
Lucas Lion <lucaslion7@gmail.com>
2025-05-19 15:18:15 +0200

    essai sur config pour images externes

--- services/frontend/next.config.ts
+++ services/frontend/next.config.ts
@@ -5,7 +5,17 @@ const nextConfig: NextConfig = {
 	middleware: true,
   env: {
     NEXT_PUBLIC_HOSTNAME: process.env.NEXT_PUBLIC_HOSTNAME,
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
